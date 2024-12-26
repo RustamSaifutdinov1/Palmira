532b51e8db7994d0059bb32aec13bda8a68ad2d5
Rustam <r.saifutdinov@car-corp.ru>
2024-12-26 15:20:26 +0300

    create about and post banner blocks

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,30 @@ $(document).ready(function () {
     /*
         Баннер на главной странице
      */
-    var SwiperMainPage = new Swiper(".main-page-banner", {
-        effect: "fade",
-        crossFade: "true",
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: "true",
-        },
-    });
+    if (window.innerWidth > 768) {
+        SwiperMainPage = new Swiper(".main-page-banner", {
+            effect: "fade",
+            crossFade: "true",
+            autoplay: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: "true",
+            },
+        });
+    }
+    $(window).on("resize", function (event) {
+        if (window.innerWidth > 768) {
+            SwiperMainPage = new Swiper(".main-page-banner", {
+                effect: "fade",
+                crossFade: "true",
+                autoplay: true,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: "true",
+                },
+            });
+        }else{
+            SwiperMainPage.destroy()
+        }
+    })
 });
